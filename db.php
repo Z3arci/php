a3827601bfe63666c636614140b48ddfef27f366
Alexey <144210511+Z3arci@users.noreply.github.com>
2024-11-18 13:24:47 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "root";
+$servername = "";
+$username = "";
+$password = "";
 $dbname = "registeratt";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
